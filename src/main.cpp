49b22353d336d6a8a9bc6e558c497e22321a385f
MattR2718 <matthew.ranson2718@gmail.com>
2023-07-12 19:12:40 +0100

    setup sfml and imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+
 int main(){
-    std::cout <<"HELLO WORLD\n";
+    const int WIDTH = sf::VideoMode::getDesktopMode().width;
+    const int HEIGHT = sf::VideoMode::getDesktopMode().height;
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "RaycastRenderer");
+    ImGui::SFML::Init(window);
+    
+    sf::Clock deltaClock;
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            ImGui::SFML::ProcessEvent(window, event);
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        ImGui::SFML::Update(window, deltaClock.restart());
+        window.clear(sf::Color::Black);
+
+        ImGui::ShowDemoWindow();
+
+
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
 }
