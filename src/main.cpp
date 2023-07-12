3054d3fcd03ef6adb618dddc1f24bb162a4353b7
MattR2718 <matthew.ranson2718@gmail.com>
2023-07-12 20:58:18 +0100

    setup fps

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string>
+#include <chrono>
 
 #include <SFML/Graphics.hpp>
 #include <imgui.h>
 #include <imgui-SFML.h>
+
+#include "imgui_setting.h"
 
 int main(){
     const int WIDTH = sf::VideoMode::getDesktopMode().width;
@@ -10,8 +14,16 @@ int main(){
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "RaycastRenderer");
     ImGui::SFML::Init(window);
     
+    std::chrono::high_resolution_clock::time_point start;
+    std::chrono::high_resolution_clock::time_point end;
+    float fps;
+
+    set_imgui_theme();
+
     sf::Clock deltaClock;
     while (window.isOpen()){
+        start = std::chrono::high_resolution_clock::now();
+
         sf::Event event;
         while (window.pollEvent(event)){
             ImGui::SFML::ProcessEvent(window, event);
@@ -24,6 +36,14 @@ int main(){
 
         ImGui::ShowDemoWindow();
 
+
+        end = std::chrono::high_resolution_clock::now();
+        fps = (float)1e9 / (float)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        ImGui::Begin("FPS", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar);
+        ImGui::SetWindowFontScale(2.5);
+        ImGui::SetWindowPos(ImVec2{ WIDTH - ImGui::GetWindowWidth(), 0 });
+        ImGui::Text(std::to_string(fps).c_str());
+        ImGui::End();
 
         ImGui::SFML::Render(window);
         window.display();
